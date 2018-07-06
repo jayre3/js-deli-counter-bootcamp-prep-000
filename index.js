e0a12c119084a41931c51613704075e2fbe422ef
Jay Singh <jeewandeep.singh@gmail.com>
2018-07-06 18:15:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  var position = katzDeliLine.lengthh;
+  return "Welcome, $[name]. You are number $[position] in the line."
+}
