b8cc0f3303242b55cd5b9d4a2dab540580bf839d
Jay Singh <jeewandeep.singh@gmail.com>
2018-07-06 18:32:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function nowServing(katzDeliLine) {
+function currentLine(katzDeliLine) {
   var line = [];
   var i = 0;
+
+  while(i<katzDeliLine.length) {
+    line.push(' ' +[i + 1]+ '. ' +katzDeliLine[i]);
+    i++;
+  }
+  if(i === 0){
+    return "The line is currently empty.";
+  }
+  else {
+    return "The line is currently:" +line;
+  }
